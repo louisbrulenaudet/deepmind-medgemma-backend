6e2994d4d54fddf3c0ab2c1a1e13fdbe2f7927c1
Louis Brulé Naudet <louisbrulenaudet@icloud.com>
2025-07-05 12:07:09 +0200

    feat(XXXX): config for chroma.

--- app/api/v1/endpoints/__init__.py
+++ app/api/v1/endpoints/__init__.py
@@ -1,4 +1,5 @@
 from .chat import router as sync_router
 from .clinical_trial_router import router as clinical_trial_router
+from .websearch import router as websearch_router
 
-__all__ = ["sync_router", "clinical_trial_router"]
+__all__ = ["sync_router", "clinical_trial_router", "websearch_router"]
--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,13 @@
-from __future__ import annotations
-
 import time
 
 from pydantic import Field
+from pydantic.functional_validators import model_validator
 from pydantic_settings import BaseSettings
 from pydantic_settings.main import SettingsConfigDict
+
+import chromadb
+from chromadb.config import Settings as ChromadbSettings
+from chromadb.utils import embedding_functions
 
 
 class Settings(BaseSettings):
@@ -22,6 +25,32 @@ class Settings(BaseSettings):
     google_api_default_method: str = "generateContent"
     google_default_model: str = "gemini-1.5-flash-latest"
     static_files_dir: str = Field(default="static", alias="STATIC_FILES_DIR")
+    chroma_client: None = None
+    chroma_collection: None = None
+
+    @model_validator(mode="after")
+    def initialize_chroma_client(self) -> "Settings":
+        self.chroma_client = chromadb.PersistentClient(
+            path="chromadb", settings=ChromadbSettings(anonymized_telemetry=False)
+        )
+
+        print("ChromaDB client initialized successfully. Downloading embedding model...")  # noqa: T201
+
+        sentence_transformer_ef = (
+            embedding_functions.SentenceTransformerEmbeddingFunction(
+                model_name="thomas-sounack/BioClinical-ModernBERT-base",
+                device="mps",
+                trust_remote_code=True,
+            )
+        )
+
+        print("Embedding model downloaded successfully.")  # noqa: T201
+
+        self.chroma_collection = self.chroma_client.get_or_create_collection(
+            name="clinical_trials", embedding_function=sentence_transformer_ef
+        )
+
+        return self
 
     model_config = SettingsConfigDict(env_file=".env")
 
